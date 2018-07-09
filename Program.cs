2dc673201d0d877d97d264f705b24a35bfada016
micsipin <39287410+micsipin@users.noreply.github.com>
2018-07-09 13:53:42 -0700

    Add .CS
    Intro: Welcome to Acme

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            DateTime time = DateTime.Now;             // Use current time.
+            string format = "MMM ddd d HH:mm yyyy";   // Use this format.
+            Console.WriteLine(time.ToString(format)); // Write to console.
+            Console.WriteLine("");
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, we’re “accounting” on you!");
+            Console.Read();
+        }
+    }
+}

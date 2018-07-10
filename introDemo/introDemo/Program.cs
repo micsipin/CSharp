dbfcaf5880c02f2aba0bea41cb8c3c1daf871ea8
micsipin <39287410+micsipin@users.noreply.github.com>
2018-07-10 12:03:18 -0700

    Intro Demo project
    console intro

---
+++ introDemo/introDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            DateTime time = DateTime.Now;            
+            string format = "MMM ddd d HH:mm yyyy";  
+            Console.WriteLine(time.ToString(format)); 
+            Console.WriteLine("");
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, we’re “accounting” on you!");
+            Console.Read();
+        }
+    }
+}

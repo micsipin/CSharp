476a46f992639f48fc5a57cd948fd0caaae4395a
micsipin <39287410+micsipin@users.noreply.github.com>
2018-08-02 19:08:37 -0700

    List Divide Try and Catch Exercise

---
+++ ListDivideExercise/ListDivideExercise/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace ListDivideExercise
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                List<int> dList = new List<int>();
+                dList.Add(20);
+                dList.Add(33);
+                dList.Add(14);
+                Console.WriteLine("pick a number");
+                int uNum = Convert.ToInt32(Console.ReadLine());
+                // Loop through List with foreach.
+                foreach (int dNum in dList)
+                {
+                    System.Console.WriteLine(dNum / uNum);
+                }
+                
+            } 
+           catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+
+            
+        }
+    }
+}

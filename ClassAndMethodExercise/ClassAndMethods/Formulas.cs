682fc82f79da38318ffb3d8ed46420e49bb78822
micsipin <39287410+micsipin@users.noreply.github.com>
2018-08-06 11:56:48 -0700

    Updated to one class with three methods.

---
+++ ClassAndMethodExercise/ClassAndMethods/Formulas.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClassAndMethods
+{
+    class Formulas
+    {
+        public int num1 { get; set; }
+        public int num2 { get; set; }
+
+        public int Addition(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public int Subtraction(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        public int Multiplication(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+    }
+}
---
+++ ClassAndMethodExercise/ClassAndMethods/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClassAndMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type in a number.");
+            int numO = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Type in another number.");
+            int numT = Convert.ToInt32(Console.ReadLine());
+
+            Formulas addition = new Formulas();
+            Formulas subtraction = new Formulas();
+            Formulas multiplication = new Formulas();
+            addition.num1 = numO;
+            addition.num2 = numT;
+            Console.WriteLine("Addition total");
+            Console.WriteLine(addition.num1 + addition.num2);
+            subtraction.num1 = numO;
+            subtraction.num2 = numT;
+            Console.WriteLine("Subtraction total");
+            Console.WriteLine(subtraction.num1 - subtraction.num2);
+            multiplication.num1 = numO;
+            multiplication.num2 = numT;
+            Console.WriteLine("Multiplication total");
+            Console.WriteLine(multiplication.num1 * multiplication.num2);
+         
+            Console.ReadLine();
+            
+
+        }
+    }
+}

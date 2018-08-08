68b42fab51d9d55c7fc121b4a614ef07e3ff671d
micsipin <39287410+micsipin@users.noreply.github.com>
2018-08-07 18:33:09 -0700

    Class Method Type

---
+++ ClassMethodType/ClassMethodType/MathOp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+    
+namespace ClassMethodType
+{
+    class MathOp
+    {
+        public int Num1  { get; set; }
+        public decimal Num2 { get; set; }
+        public decimal Num3 { get; set; }
+
+        public int AddDivide(int Num1)
+        {
+            return (Num1 + Num1) / Num1;
+        }
+        public decimal  DivideAdd(decimal Num2)
+        { 
+            return (Num2 / Num2) + Num2;
+        }
+        public int MultiAddDivide(Int32 Num3)
+        {
+            return (Num3 * Num3) + Num3 / Num3;
+        }
+    }
+}
---
+++ ClassMethodType/ClassMethodType/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClassMethodType
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                Console.WriteLine("Type in a number.");
+                int Num1 = Convert.ToInt32(Console.ReadLine());
+                MathOp addDivide = new MathOp();
+                addDivide.Num1 = Num1;
+                int calculateInt = addDivide.AddDivide(Num1);
+                Console.WriteLine("(" + Num1 + "+" + Num1 + ")/" + Num1 + " = " + calculateInt);
+                Console.WriteLine();
+
+                Console.WriteLine("Type in a decimal number.");
+                decimal Num2 = Convert.ToDecimal(Console.ReadLine());
+                MathOp divideAdd = new MathOp();
+                divideAdd.Num2 = Num2;
+                decimal calculateDec = divideAdd.DivideAdd(Num2);
+                Console.WriteLine("(" + Num2 + "/" + Num2 + ")+" + Num2 + " = " + calculateDec);
+                Console.ReadLine();
+
+                Console.WriteLine("Type in a number.");
+                string Num3 = Console.ReadLine();
+                MathOp MultiAddDivide = new MathOp();
+                MultiAddDivide.Num3 = Convert.ToInt32(Num3);
+                decimal calculateString = MultiAddDivide.MultiAddDivide(Convert.ToInt32(Num3));
+                Console.WriteLine("(" + Num3 + "*" + Num3 + ")+" + Num3 + " / " + Num3 + " = " + calculateString);
+
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}

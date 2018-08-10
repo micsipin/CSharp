86574b6c9d36204592fbf5fcf2ff88da833d8fb9
micsipin <39287410+micsipin@users.noreply.github.com>
2018-08-09 21:58:37 -0700

    Optional Method

---
+++ OptionMethod/OptionMethod/ClassOption.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OptionMethod
+{
+    public class ClassOption
+    {
+        /*1. Create a class. In that class, create a method that
+         * takes two integers as parameters. Make one of them optional.
+         * Have the method do a math operation and return a integer result.
+         */ 
+        public static int Option (int num1, int num2 )
+        {
+            int results = num1 * Convert.ToInt32(num2);
+            return results;
+        }
+        
+    }
+}
---
+++ OptionMethod/OptionMethod/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace OptionMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*2.In the Main() method of the console app, instantiate the class.
+             */
+            ClassOption Option = new ClassOption();
+
+            /*3.Ask the user to input two numbers, one at a time.Let them know 
+             * they need not enter anything for the second number.
+             */
+            Console.WriteLine("Please type in a number.");
+            int num1 = Int32.Parse(Console.ReadLine());
+            int uInput = num1;
+            Console.WriteLine("**OPTIONAL** \n You may pick another number. If not, the default is chosen.");
+            int numX = 40;
+            string iInput = Console.ReadLine();
+                      
+            /*4. Call the method in the class, passing in the one or two
+            * numbers entered.
+            */
+            if (string.IsNullOrEmpty(iInput))
+            {
+                Console.WriteLine(ClassOption.Option(uInput, Convert.ToInt32(numX)));
+            }
+            else
+            {
+                Console.WriteLine(ClassOption.Option(uInput, Convert.ToInt32(iInput)));
+            }
+            //5.Try various combinations of numbers on the code, including having no second number.
+
+            Console.ReadLine();
+        }
+    }
+}

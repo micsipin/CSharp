ca3bba6a3c863200a0162fbe82a7f4d75439c248
micsipin <39287410+micsipin@users.noreply.github.com>
2018-08-16 09:27:19 -0700

    Abstract and Inheritance

---
+++ Abstract/Abstract/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abstract
+{
+    class Employee : Person
+    {
+        public override void SayName()
+        {
+            Console.WriteLine($"Name: [{firstName} {lastName}]");
+        }
+    }
+}
+
---
+++ Abstract/Abstract/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abstract  
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee() { firstName = "Sample", lastName = "Student" };
+            emp.SayName();
+            Console.ReadLine();
+
+
+        }
+
+    }
+}

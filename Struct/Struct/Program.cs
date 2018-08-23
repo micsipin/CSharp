9098c0d43e2f5da9ee926f7a7decb2d1c917a01e
micsipin <39287410+micsipin@users.noreply.github.com>
2018-08-22 23:23:57 -0700

    Struct Demo

---
+++ Struct/Struct/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Struct
+{
+    public struct Number
+    {
+        public int Amount { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number num = new Number()
+            {
+                Amount = 80085
+            };
+            Console.WriteLine(num.Amount);
+            Console.ReadLine();
+        }
+    }
+}

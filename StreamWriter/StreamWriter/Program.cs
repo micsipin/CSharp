ce628dd2547b68ddf6d44c1854bf0d3bbcd35787
micsipin <39287410+micsipin@users.noreply.github.com>
2018-09-04 15:22:24 -0700

    StreamWriter

---
+++ StreamWriter/StreamWriter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace StreamWriterNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input some numbers");
+            string uInput = Console.ReadLine();
+            
+
+            File.WriteAllText(@"C:\\Users\\Tech Academy Student\\Desktop\\log.txt", uInput);
+            string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "consoleLogged.txt")))
+            {
+                outputFile.WriteLine(uInput);
+            }
+            Console.WriteLine("Your input, " + uInput + "  was printed to a .txt file in your MyDocuments folder, under consoleLogged.txt");
+            Console.ReadLine();
+        }
+    }
+}
+

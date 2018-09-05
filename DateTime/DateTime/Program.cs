1697ba407e189c9d8c1e6089d556040dc9aff12a
micsipin <39287410+micsipin@users.noreply.github.com>
2018-09-04 18:45:05 -0700

    DateTime + Hours

---
+++ DateTime/DateTime/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //DateTime now = DateTime.Now;
+            //Console.WriteLine("Time is now: ",now);
+            //Console.WriteLine("Please input a number");
+            //int uInput = Convert.ToInt32(Console.ReadLine());
+            //DateTime uInputDate = (now.AddDays(uInput));
+            //Console.WriteLine(uInputDate);
+            //Console.ReadLine();
+
+
+            DateTime now = DateTime.Now;
+            Console.WriteLine("Time is now: ", now);
+            Console.WriteLine("Please input a number");
+            int uInput = Convert.ToInt32(Console.ReadLine());
+            DateTime uInputHour = (now.AddHours(uInput));
+            Console.WriteLine("in " + uInput + " hour(s), the time will be " + uInputHour);
+            Console.ReadLine();
+        }
+    }
+}

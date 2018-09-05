d86ba8c591d7c3f2cc587829d27324b10b62d1cd
micsipin <39287410+micsipin@users.noreply.github.com>
2018-09-05 16:18:40 -0700

    Constructor Chaining

---
+++ ConstructorChaining1/ConstructorChaining1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorChaining1
+{
+    class A
+    {
+        public A()
+        {
+            Console.WriteLine("Constructor A");
+        }
+        public A(string z)
+        {
+            Console.WriteLine("Constructor A with param = {0}",z); 
+        }
+        public A(string z, string y)
+        {
+            Console.WriteLine("Constructor A with param = {0}",z,y);
+        }
+    }
+    class B:A
+    {
+        public B():base()
+        {
+            Console.WriteLine("Constructor B");
+        }
+        public B(string z):base(z)
+        {
+            Console.WriteLine("Constructor B with para = {0}", z);
+        }
+        public B(string z, string y):base(z,y)
+        {
+            Console.WriteLine("Constructor B with params = {0} & {1}", z, y);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            B b = new B();
+            B b2 = new B("param 1", "param 2");
+            Console.ReadLine();
+        }
+    }
+}

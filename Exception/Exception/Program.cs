26abf4df8770e9d2039ecad0c7a1b523e2917a2e
micsipin <39287410+micsipin@users.noreply.github.com>
2018-09-06 14:21:12 -0700

    Exceptions

---
+++ Exception/Exception/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("How old are you, Human?");
+                int uAge = Convert.ToInt32(Console.ReadLine());
+                if (uAge < 1)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                DateTime yrBorn = DateTime.Now.AddYears(-uAge);
+                Console.WriteLine("You were created in the year " + yrBorn.Year + ".");
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("You're too young to use this computer..unless young Jedi");
+            }
+            catch(System.Exception)
+            {
+                Console.WriteLine("Cannot Compute.. \nPlease enter a Number besides 0");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
